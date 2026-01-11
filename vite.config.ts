44a96168b5cbf72b6bcc0c392586ddffdc7bd773
jkgithub02 <jkwork02@gmail.com>
2026-01-12 00:13:45 +0800

    added production boolean check

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,20 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/enactus-unm-hub-main/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  return {
+    base: isProd ? '/enactus-unm/' : '/',
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
