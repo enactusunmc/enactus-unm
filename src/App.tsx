929a42aeb0c86fc70d5e899198929f30aba2d543
jkgithub02 <jkwork02@gmail.com>
2026-01-12 03:53:07 +0800

    reattempting deployment

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/enactus-unm">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/team" element={<Team />} />
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode, command }) => {
   const isProd = command === 'build';
 
   return {
-    base: isProd ? './' : '/',
+    base: isProd ? '/enactus-unm/' : '/',
     server: {
       host: "::",
       port: 8080,
